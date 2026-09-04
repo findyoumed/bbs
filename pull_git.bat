40bd433efb3fdcfaec64e37b6b502a485d36af29
findyoumed <postnews@daum.net>
2026-09-04 17:46:56 +0900

    update

--- pull_git.bat
+++ pull_git.bat
@@ -4,6 +4,10 @@ echo ==========================================
 echo   Git Pull Helper Script
 echo ==========================================
 echo.
+:: [LOG: 20260904_1747]
+echo [안내] Git 가져오기를 시작합니다. 잘못 누르셨다면 창을 닫거나 Ctrl+C를 누르세요.
+pause
+echo.
 
 :: Check if .git folder exists to decide clone or pull
 if not exist ".git" (
